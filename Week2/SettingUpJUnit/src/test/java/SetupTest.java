ff5d912a3d69ff97569818a1f510feb01c29aeea
Ishant Varshney <ishant65varshney@gmail.com>
2026-07-22 21:38:57 +0530

    commit

---
+++ Week2/SettingUpJUnit/src/test/java/SetupTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+public class SetupTest {
+
+    @Test
+    public void testEnvironmentSetup() {
+        int expectedShield = 150;
+        int actualShield = 100 + 50;
+    }
+}
